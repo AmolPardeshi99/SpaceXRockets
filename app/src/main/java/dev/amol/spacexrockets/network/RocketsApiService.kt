07c93f65055c01fef94fe88950c5184cf6731fee
AmolPardeshi99 <pardeshiamol772@gmail.com>
2023-02-20 19:43:41 +0530

    - added assets for documentation

--- app/src/main/java/dev/amol/spacexrockets/network/RocketsApiService.kt
+++ app/src/main/java/dev/amol/spacexrockets/network/RocketsApiService.kt
@@ -4,12 +4,13 @@ import dev.amol.spacexrockets.model.network.SpacexRocketDto
 import dev.amol.spacexrockets.utils.Constants.ENDPOINT_ROCKETS_DATA
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RocketsApiService {
 
     @GET(ENDPOINT_ROCKETS_DATA)
     suspend fun getAllRocketsData():Response<List<SpacexRocketDto>>
 
-    @GET("$ENDPOINT_ROCKETS_DATA {id}")
-    suspend fun getSpecificRocketData(id:String):Response<SpacexRocketDto>
+    @GET("$ENDPOINT_ROCKETS_DATA{id}")
+    suspend fun getSpecificRocketData(@Path("id") id:String):Response<SpacexRocketDto>
 }
